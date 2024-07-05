d3cdad9c8beb9c21bfd5b2558839f0726f09d015
Spectre <elouan.fare@protonmail.ch>
2024-07-05 11:33:37 +0200

    remove target

--- server/target/debug/deps/server-6fadb03198841212.d
+++
@@ -1,5 +0,0 @@
-/home/spectre/Bureau/rust/mc_server_rust/server/target/debug/deps/libserver-6fadb03198841212.rmeta: src/main.rs
-
-/home/spectre/Bureau/rust/mc_server_rust/server/target/debug/deps/server-6fadb03198841212.d: src/main.rs
-
-src/main.rs:
