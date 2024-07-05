9efee850b4b2623f02601470fda31365c7320be0
Spectre <elouan.fare@protonmail.ch>
2024-07-05 11:27:53 +0200

    change again gitignore

---
+++ server/target/debug/deps/server-fa5245b30189cfc1.d
@@ -0,0 +1,5 @@
+/home/spectre/Bureau/rust/mc_server_rust/server/target/debug/deps/libserver-fa5245b30189cfc1.rmeta: src/main.rs
+
+/home/spectre/Bureau/rust/mc_server_rust/server/target/debug/deps/server-fa5245b30189cfc1.d: src/main.rs
+
+src/main.rs:
