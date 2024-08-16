21001d85c59bebdfac94e86eafa222f04419a63d
Spectre <elouan.fare@protonmail.ch>
2024-08-16 17:32:57 +0200

    add struct for server_properties

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -21,6 +21,84 @@ use std::path::Path;
 /// println!("{difficulty}");
 /// println!("{max_players}");
 /// ```
+///
+enum Difficulty {
+    EASY,
+    NORMAL,
+    HARD,
+}
+enum Gamemode{
+    ADVENTURE,
+    SURVIVAL,
+    CREATIVE,
+    SPECTATOR,
+}
+enum WorlPreset{
+    NORMAL,
+    FLAT,
+    LARGEBIOMES,
+    AMPLIFIED,
+    SINGLEBIOMESURFACE,
+}
+//!TODO generator_settings
+//!Todo text-filtering-config
+struct Settings{
+    enable_jmx_monitoring: bool,
+    rcon_port: u16,
+    level_seed: Option<i64>,
+    gamemode: Gamemode,
+    enable_command_block: bool,
+    enable_query: bool,
+    enforce_secure_profile: bool,
+    level_name: Option<String>,
+    motd: Option<String>,
+    query_port: u16,
+    pvp: bool,
+    generate_structures: bool,
+    max_chained_neighbor_updates: Option<i32>,
+    difficulty: Difficulty,
+    network_compression_threshold: i32,
+    max_tick_time: i64,
+    require_resource_pack: bool,
+    use_native_transport: bool,
+    max_players: u32,
+    online_mode: bool,
+    enable_status: bool,
+    allow_flight: bool,
+    initial_disabled_packs: Option<String>,
+    broadcast_rcon_to_ops: bool,
+    view_distance: u8,
+    server_ip: Option<String>,
+    resource_pack_prompt: Option<String>,
+    allow_nether: bool,
+    server_port: u16,
+    enable_rcon: bool,
+    sync_chunk_writes: bool,
+    op_permission_level: u8,
+    prevent_proxy_connections: bool,
+    hide_online_players: bool,
+    resource_pack: Option<String>,
+    entity_broadcast_range_percentage: u8,
+    simulation_distance: u8,
+    rcon_password: Option<String>,
+    player_idle_timeout: i32,
+    force_gamemode: bool,
+    rate_limit: u32,
+    hardcore: bool,
+    white_list: bool,
+    broadcast_console_to_ops: bool,
+    spawn_npcs: bool,
+    spawn_animals: bool,
+    log_ips: bool,
+    function_permission_level: u8,
+    initial_enabled_packs: String,
+    level_type: WorlPreset,
+    spawn_monsters: bool,
+    enforce_whitelist: bool,
+    spawn_protection: u16,
+    resource_pack_sha1: Option<String>,
+    max_world_size: u32,
+}
 pub fn read(filepath: &Path) -> std::io::Result<Properties> {
     let file = File::open(filepath)?;
     let mut reader = BufReader::new(file);
