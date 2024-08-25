1c2d15b6eb61ab7683179f3c78a2ff2f8e902154
spectre <elouan.fare@protonmail.ch>
2024-08-25 01:08:53 -0700

    oops forgot to commit this file....

--- src/file_folder_parser/mod.rs
+++ src/file_folder_parser/mod.rs
@@ -1,29 +1,28 @@
 use std::fs::File;
-use std::io::{self, BufRead, Write};
+use std::io::{self, Write ,BufRead};
 use std::path::Path;
+use colored::Colorize;
 mod utils;
 
-///
-pub fn create_server_properties(
-    content: &str,
-    file_path: &'static str,
-    formatted_time: &str,
-) -> io::Result<()> {
+pub fn create_server_properties(content:&str,file_path:&'static str,formatted_time:&str) -> io::Result<()> {
     let path = Path::new(file_path);
     let final_input = format!(
         "#Minecraft server properties\n#{}\n{}",
-        formatted_time, content,
+        formatted_time,
+        content,
     );
-    // Verify if the file already exists
-    if utils::check_file(path) == false {
+    // verify if the file already exist
+    if utils::check_file(path) == false{
         let mut file = File::create(path)?;
         file.write_all(final_input.as_bytes())?;
-        println!("The file \"{}\" has been created.", file_path);
+        println!("The file \"{}\" has been created.",file_path.blue())
     }
     Ok(())
-} //test passed
+}//test passed
 
-pub fn create_eula(file_path: &'static str, formatted_time: &str) -> io::Result<()> {
+pub fn create_eula(file_path:&'static str,formatted_time:&str) -> io::Result<()> {
+
+
     let final_input = format!(
         "#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://aka.ms/MinecraftEULA).\n#{}\neula=false",
         formatted_time,
@@ -31,17 +30,18 @@ pub fn create_eula(file_path: &'static str, formatted_time: &str) -> io::Result<
     );
 
     let path = Path::new(file_path);
-    if utils::check_file(path) == false {
+    if utils::check_file(path) == false{
         let mut file = File::create(path)?;
         file.write_all(final_input.as_bytes())?;
-        println!("Creation of the file {}", file_path)
+        println!("Creation of the file {}",file_path.red())
     }
 
     Ok(())
 }
 
+
 pub fn check_eula(path: &'static str) -> bool {
-    println!("Reading the file {}…", path);
+    println!("Reading the file {}…",path);
     if let Ok(file) = File::open(Path::new(path)) {
         let reader = io::BufReader::new(file);
 
@@ -57,3 +57,4 @@ pub fn check_eula(path: &'static str) -> bool {
 
     false
 }
+
