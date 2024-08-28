7ddd414e30a9915bce367e689e81893fd177af3d
spectre <elouan.fare@protonmail.ch>
2024-08-28 10:05:52 +0200

    add a fn which return a non formatted time

--- src/time/mod.rs
+++ src/time/mod.rs
@@ -6,3 +6,7 @@ pub fn get_formatted_time() -> String {
     let local_time: DateTime<Local> = now.with_timezone(&Local); // Convert to local machine time
     local_time.format("%a %b %d %H:%M:%S %Y").to_string() // Format time
 }
+pub fn get_time() -> DateTime<Local> {
+    let now = Utc::now();
+    now.with_timezone(&Local) // Convert to local machine time
+}
